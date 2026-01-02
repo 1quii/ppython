b2deed31f188491cfe39eac18e60a4afef8cfb90
1quii <diego187hv@gmail.com>
2026-01-02 11:02:33 +0100

    calculator2

--- calculator.py
+++ calculator.py
@@ -1,5 +1,9 @@
-x=int(input("What is x? "))
-y=int(input("What is y? "))
-print("x + y =", x + y)
+x=float(input("What is x? "))
+y=float(input("What is y? "))
+z=round(x+y,2)
+print("x + y =", z)
 # calculator.py
-#if a need a float number, only change int to float
+#if a need a int number, only change int to float
+
+print(f"this is = {z:,}")
+#if i want to add commas in large numbers
