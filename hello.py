debecb6e857ffed348fd0aa19d01764ddfeaac8f
1quii <diego187hv@gmail.com>
2026-01-04 18:55:02 +0100

    0.2

--- hello.py
+++ hello.py
@@ -5,10 +5,16 @@ def greet_user():
     name=input("Enter your name: ").strip().title()
     #Say hello to the user
     print("Hello,", name)   
-"""
+
 def hello(para="Usuario"):
     print("Hello,", para)
     
 hello()
-    name=input("Enter your name: ").strip().title()
-    hello(name) 
+name=input("Enter your name: ").strip().title()
+hello(name) 
+"""
+def main():
+    nombre=input("Whats your name? ").strip().title()
+    hello(nombre)
+
+def hello
