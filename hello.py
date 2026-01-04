bad8479d6492cc29ec229a48e9b9d10df083f7e6
1quii <diego187hv@gmail.com>
2026-01-04 18:50:42 +0100

    functions

---
+++ hello.py
@@ -0,0 +1,14 @@
+"""
+#use def to define a function
+def greet_user():
+    #Ask for user their name 
+    name=input("Enter your name: ").strip().title()
+    #Say hello to the user
+    print("Hello,", name)   
+"""
+def hello(para="Usuario"):
+    print("Hello,", para)
+    
+hello()
+    name=input("Enter your name: ").strip().title()
+    hello(name) 
