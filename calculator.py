7fd02cf135873d9eaeb534c101320bde076d8ffd
1quii <diego187hv@gmail.com>
2026-01-07 11:20:36 +0100

    calculator3

--- calculator.py
+++ calculator.py
@@ -6,4 +6,12 @@ print("x + y =", z)
 #if a need a int number, only change int to float
 
 print(f"this is = {z:,}")
-#if i want to add commas in large numbers
+# this will add comma as thousand separator
+
+# pedir un numero e imprimir su cuadrado
+def main():
+    x= int(input("Cual es el numero? "))
+    print("El cuadrado es, ", subir(x))
+def subir(n):
+    return n**2
+main()
